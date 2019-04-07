5ba8df29d9eba3c5e5f205af562ac601a15c9121
Hao Yang <hxy313@case.edu>
2019-04-07 16:32:04 -0400

    added assets for dog and dog is currently able to jump

---
+++ src/Assets/Scripts/dogController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dogController : MonoBehaviour
+{
+   
+    private Rigidbody2D myRigidBody;
+    public float jumpForce = 500f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonUp("Jump"))
+        {
+            myRigidBody.AddForce(transform.up * jumpForce);
+        }
+    }
+}
