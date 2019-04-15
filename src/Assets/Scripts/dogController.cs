86fa0f8ace5ea361e17556e8457a6fc3c2b44e9a
Mingxuan Ju <mxj255@case.edu>
2019-04-15 15:50:06 -0400

    energy features and dog treats

--- src/Assets/Scripts/dogController.cs
+++ src/Assets/Scripts/dogController.cs
@@ -1,17 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class dogController : MonoBehaviour
 {
    
     private Rigidbody2D myRigidBody;
     public float jumpForce = 500f;
     private Animator myAinm;
-
+    public float energyRemained = 100;
     // Start is called before the first frame update
     void Start()
-    {
+    {   
         myRigidBody = GetComponent<Rigidbody2D>();
         myAinm = GetComponent<Animator>();
     }
@@ -19,9 +18,10 @@ public class dogController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetButtonUp("Jump"))
+        if(Input.GetButtonUp("Jump") && energyRemained>=10)
         {
             myRigidBody.AddForce(transform.up * jumpForce);
+            energyRemained = energyRemained - 10;
         }
 
         myAinm.SetFloat("vVelocity", myRigidBody.velocity.y);
@@ -29,10 +29,28 @@ public class dogController : MonoBehaviour
 
     void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.collider.gameObject.layer == LayerMask.NameToLayer("Enemy"))
+        Vector3 rot = transform.eulerAngles;
+        if (collision.collider.gameObject.layer == LayerMask.NameToLayer("Enemy"))
         {
             Application.LoadLevel(Application.loadedLevel);
 
         }
+        if (collision.collider.gameObject.layer == LayerMask.NameToLayer("Food1"))
+        {
+            energyRemained += 30;
+            Destroy(collision.gameObject);
+            transform.eulerAngles = new Vector3(360-rot.x, 0, 0);
+        }
+        if (collision.collider.gameObject.layer == LayerMask.NameToLayer("Food2"))
+        {
+            energyRemained += 50;
+            Destroy(collision.gameObject);
+            transform.eulerAngles = new Vector3(360-rot.x, 0, 0);
+        }
+    }
+
+    private void OnGUI()
+    {
+        GUI.Label(new Rect(0, 0, 100, 100), "Energy Remained: "+ energyRemained);
     }
 }
